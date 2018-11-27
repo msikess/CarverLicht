98bfe176f73acca631eaaf62b09f385bc7dd8b87
Mariam <nadiradz@grinnell.edu>
2018-11-27 17:14:31 -0600

    Updated version with all the sprites

---
+++ Assets/SimRigShift.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimRigShift : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    public float amplitudeX = 10.0f;
+    public float frequencyX = .05f;
+    float rotationY = 0.0f;
+    float rotationX = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.rotation = Quaternion.Euler(rotationX, rotationY, amplitudeX * Mathf.Sin(frequencyX * Time.frameCount));
+
+    }
+}
---
+++ Assets/Throw.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour {
+
+    public float speed = 10;//MAKE SPEED BASED ON CONTROLLER POSITION OVER TIME?
+    public bool canHold = true;
+    public GameObject ball;
+    public Transform guide;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //CHANGE TO BE BASED ON CLICK ON CONTROLLER? Or maybe don't need if ball reappears in hand 
+        {
+            if (!canHold)
+                throw_drop();
+            else
+                Pickup();
+        }//mause If
+
+        if (!canHold && ball)
+            ball.transform.position = guide.position;
+
+    }//update
+
+    //We can use trigger or Collision
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "ball")
+            if (!ball) // if we don't have anything holding
+                ball = col.gameObject;
+    }
+
+    //We can use trigger or Collision
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "ball")
+        {
+            if (canHold)
+                ball = null;
+        }
+    }
+
+
+    private void Pickup()
+    {
+        if (!ball)
+            return;
+
+        //We set the object parent to our guide empty object.
+        ball.transform.SetParent(guide);
+
+        //Set gravity to false while holding it
+        ball.GetComponent<Rigidbody>().useGravity = false;
+
+        //we apply the same rotation our main object (Camera) has.
+        ball.transform.localRotation = transform.rotation;
+        //We re-position the ball on our guide object 
+        ball.transform.position = guide.position;
+
+        canHold = false;
+    }
+
+    private void throw_drop()
+    {
+        if (!ball)
+            return;
+
+        //Set our Gravity to true again.
+        ball.GetComponent<Rigidbody>().useGravity = true;
+        // we don't have anything to do with our ball field anymore
+        ball = null;
+        //Apply velocity on throwing
+        guide.GetChild(0).gameObject.GetComponent<Rigidbody>().velocity = transform.forward * speed;
+
+        //Unparent our ball
+        guide.GetChild(0).parent = null;
+        canHold = true;
+    }
+}//class
